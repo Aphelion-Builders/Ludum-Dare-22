15a9abcbd335b4126200a49fac340e5c531f5587
MinerCraftGuy <flyaway78.mc@gmail.com>
2011-12-17 22:30:17 +0100

    + Updated the golem sprite so it looks more like the game char

--- game.js
+++ game.js
@@ -55,8 +55,8 @@ $(function()
 						.addSprite("background1", {animation: background1, width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT})
 					.end()
 					.addGroup("actors", {width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT})
-						.addGroup("player", {posx: 0, posy: PLAYGROUND_HEIGHT-128, width: 128, height: 128})
-							.addSprite("playerBody",{animation: playerAnimation["right"], posx: 0, posy: 0, width: 128, height: 128})
+						.addGroup("player", {posx: 10, posy: 480, width: 38, height: 90})
+							.addSprite("playerBody",{animation: playerAnimation["right"], posx: 0, posy: 0, width: 38, height: 90})
 						.end()
 					.end()
 	
@@ -85,7 +85,7 @@ $(function()
 				}
 				if(jQuery.gameQuery.keyTracker[39]){ //this is right! (d)
 					var nextpos = parseInt($("#player").css("left"))+5;
-					if(nextpos < PLAYGROUND_WIDTH - 100){
+					if(nextpos < PLAYGROUND_WIDTH - 40){
 						$("#player").css("left", ""+nextpos+"px");
 					}
 				}
@@ -97,7 +97,7 @@ $(function()
 				}
 				if(jQuery.gameQuery.keyTracker[40]){ //this is down! (s)
 					var nextpos = parseInt($("#player").css("top"))+3;
-					if(nextpos < PLAYGROUND_HEIGHT - 30){
+					if(nextpos < PLAYGROUND_HEIGHT/2){
 						$("#player").css("top", ""+nextpos+"px");
 					}
 				}
@@ -124,107 +124,13 @@ $(function()
 					$("#player").css("left", ""+ posx +"px");
 				}
 			}
-			
-			//Update the movement of the enemies
-			$(".enemy").each(function(){
-					this.enemy.update($("#player"));
-					var posx = parseInt($(this).css("left"));
-					if((posx + 100) < 0){
-						$(this).remove();
-						return;
-					}
-					//Test for collisions
-					var collided = $(this).collision("#playerBody,.group");
-					if(collided.length > 0){
-						if(this.enemy instanceof Bossy){
-								$(this).setAnimation(enemies[2]["explode"], function(node){$(node).remove();});
-								$(this).css("width", 150);
-						} else if(this.enemy instanceof Brainy) {
-							$(this).setAnimation(enemies[1]["explode"], function(node){$(node).remove();});
-							$(this).css("width", 150);
-						} else {
-							$(this).setAnimation(enemies[0]["explode"], function(node){$(node).remove();});
-							$(this).css("width", 200);
-						}
-						$(this).removeClass("enemy");
-						//The player has been hit!
-						if($("#player")[0].player.damage()){
-							explodePlayer($("#player"));
-						}
-					}
-					//Make the enemy fire
-					if(this.enemy instanceof Brainy){
-						if(Math.random() < 0.05){
-							var enemyposx = parseInt($(this).css("left"));
-							var enemyposy = parseInt($(this).css("top"));
-							var name = "enemiesMissile_"+Math.ceil(Math.random()*1000);
-							$("#enemiesMissileLayer").addSprite(name,{animation: missile["enemies"], posx: enemyposx, posy: enemyposy + 20, width: 30,height: 15});
-							$("#"+name).addClass("enemiesMissiles");
-						}
-					}
-				});
-			
-			//Update the movement of the missiles
-			$(".playerMissiles").each(function(){
-					var posx = parseInt($(this).css("left"));
-					if(posx > PLAYGROUND_WIDTH){
-						$(this).remove();
-						return;
-					}
-					$(this).css("left", ""+(posx+MISSILE_SPEED)+"px");
-					//Test for collisions
-					var collided = $(this).collision(".group,.enemy");
-					if(collided.length > 0){
-						//An enemy has been hit!
-						collided.each(function(){
-								if($(this)[0].enemy.damage()){
-									if(this.enemy instanceof Bossy){
-											$(this).setAnimation(enemies[2]["explode"], function(node){$(node).remove();});
-											$(this).css("width", 150);
-									} else if(this.enemy instanceof Brainy) {
-										$(this).setAnimation(enemies[1]["explode"], function(node){$(node).remove();});
-										$(this).css("width", 150);
-									} else {
-										$(this).setAnimation(enemies[0]["explode"], function(node){$(node).remove();});
-										$(this).css("width", 200);
-									}
-									$(this).removeClass("enemy");
-								}
-							})
-						$(this).setAnimation(missile["playerexplode"], function(node){$(node).remove();});
-						$(this).css("width", 38);
-						$(this).css("height", 23);
-						$(this).css("top", parseInt($(this).css("top"))-7);
-						$(this).removeClass("playerMissiles");
-					}
-				});
-			$(".enemiesMissiles").each(function(){
-					var posx = parseInt($(this).css("left"));
-					if(posx < 0){
-						$(this).remove();
-						return;
-					}
-					$(this).css("left", ""+(posx-MISSILE_SPEED)+"px");
-					//Test for collisions
-					var collided = $(this).collision(".group,#playerBody");
-					if(collided.length > 0){
-						//The player has been hit!
-						collided.each(function(){
-								if($("#player")[0].player.damage()){
-									explodePlayer($("#player"));
-								}
-							})
-						//$(this).remove();
-						$(this).setAnimation(missile["enemiesexplode"], function(node){$(node).remove();});
-						$(this).removeClass("enemiesMissiles");
-					}
-				});
 		}
 	}, REFRESH_RATE);
 	
 	//this is where the keybinding occurs
 	$(document).keydown(function(e){
-		if(!gameOver && !playerHit){
+		if(!gameOver)
+		{
 			switch(e.keyCode)
 			{
 				case 37: //this is left! (a)
@@ -244,7 +150,8 @@ $(function()
 	});
 	//this is where the keybinding occurs
 	$(document).keyup(function(e){
-		if(!gameOver && !playerHit){
+		if(!gameOver)
+		{
 			switch(e.keyCode){
 				case 37: //this is left! (a)
 					$("#playerBody").setAnimation(playerAnimation["left"]);
