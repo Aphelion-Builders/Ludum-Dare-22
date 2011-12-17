58bbfec93600435a1d8f1a3f79f667c7630ba844
MinerCraftGuy <flyaway78.mc@gmail.com>
2011-12-17 05:48:32 +0100

    + Forgot to add the index to the list *DUH!*

--- initialise.js
+++ initialise.js
@@ -1,20 +1,27 @@
-$(function(){
-  $("#playground").playground({width: 800, height: 600, keyTracker: true});
-  game.playground = $.playground();
-  game.background = $("#sceengraph");
+$(function()
+{
+	$("#playground").playground({width: 1024, height: 768, keyTracker: true});
+	game.playground = $.playground();
+	game.background = $("#sceengraph");
+	game.state = "not-started";
 
-  game.golem = new Golem();
-  game.playground
-    .addSprite("Golem", {animation: game.farmer.getAnimation(),
-      width: game.farmer.width, height: game.farmer.height, posx: 400, posy: 300});
-  game.golem.elem = $("#golem");
-  game.golem.setPos(100, 100);
-
-  view = new GolemView();
-  registerCallbacks();
-
-  game.state = "not-started";
-
-  keyTracker = $.gameQuery.keyTracker;
-  $().setLoadBar("loading-bar", 400);
+	keyTracker = $.gameQuery.keyTracker;
+	$().setLoadBar("loading-bar", 400);
 });
+
+function registerCallbacks(){
+  game.playground
+    .registerCallback(gameLoop, JarmView.frameRate)
+    .registerCallback(function() {view.update();}, view.updateRate);
+
+  $("#start-button").click(function(){
+    game.state = "playing";
+    game.playground
+      .startGame(function(){
+        $("#welcome-screen").remove();
+      });
+  });
+
+  $(document).keypress(onKeyPress);
+  $(document).click(onClick);
+}
